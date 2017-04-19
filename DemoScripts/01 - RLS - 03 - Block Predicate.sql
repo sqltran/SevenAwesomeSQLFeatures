dab78646ddc0f3f42d04f8221706821c5f587173
Allison Benneth <allison@sqltran.org>
2017-04-18 19:56:02 -0500

    Added to RLS demos.

---
+++ DemoScripts/01 - RLS - 03 - Block Predicate.sql
@@ -0,0 +1,39 @@
+-- So how can prevent a user from updating a row in such a way
+-- that they can no longer access that data?
+-- Answer: Block predicates.
+
+-- Without the block predicate, department leader rgrayson can update fellow
+-- customer service employee 'jhalligan' to another department.
+execute as user = 'rgrayson';  -- Customer service department head
+begin transaction;
+update dbo.Employee set DepartmentCode = 'IT' where LoginName = 'jhalligan';
+select * from dbo.Employee where LoginName = 'jhalligan';
+revert;
+select * from dbo.Employee where LoginName = 'jhalligan';
+rollback transaction;
+-- 1 row affected
+
+-- Now add a block predicate.
+alter security policy EmployeeFilter
+add block predicate Security.fnEmployeeRLSPredicate (LoginName, DepartmentCode)
+on dbo.Employee after insert,
+add block predicate Security.fnEmployeeRLSPredicate (LoginName, DepartmentCode)
+on dbo.Employee after update;
+
+-- Try the same operation.
+execute as user = 'rgrayson';  -- Customer service department head
+begin transaction;
+update dbo.Employee set DepartmentCode = 'IT' where LoginName = 'jhalligan';
+select * from dbo.Employee where LoginName = 'jhalligan';
+revert;
+select * from dbo.Employee where LoginName = 'jhalligan';
+rollback transaction;
+-- Msg 33504, Level 16, State 1, Line 26
+-- The attempted operation failed because the target object 'CompanyHR.dbo.Employee' has a block predicate that conflicts with this operation. If the operation is performed on a view, the block predicate might be enforced on the underlying table. Modify the operation to target only the rows that are allowed by the block predicate.
+-- The statement has been terminated.
+
+alter security policy EmployeeFilter
+drop block predicate on dbo.Employee after insert;
+
+alter security policy EmployeeFilter
+drop block predicate on dbo.Employee after update;
---
+++ DemoScripts/01 - RLS - 04 - Information Leakage.sql
@@ -0,0 +1,27 @@
+use CompanyHR;
+go
+
+-- However, some information can be leaked.
+-- Note one person's salary:
+select * from dbo.Employee where LoginName = 'cfloyd';
+
+-- User 'jellis' cannot see this record:
+execute as user = 'jellis';  -- Customer service employee
+select * from dbo.Employee where LoginName = 'cfloyd';
+revert;
+
+-- But with a carefully crafted predicate we get some information.
+execute as user = 'jellis';  -- Customer service employee
+select * from dbo.Employee where 1.0 / (Salary - 90731.00) = 0;
+revert;
+-- No record returned, but:
+-- Msg 8134, Level 16, State 1, Line 132
+-- Divide by zero error encountered.
+
+-- Then we can start to eliminate who it is.
+execute as user = 'jellis';  -- Customer service employee
+select * from dbo.Employee where LoginName = 'lstapleton' and 1.0 / (Salary - 90731.00) = 0;
+select * from dbo.Employee where LoginName = 'sscott' and 1.0 / (Salary - 90731.00) = 0;
+select * from dbo.Employee where LoginName = 'tkidd' and 1.0 / (Salary - 90731.00) = 0;
+select * from dbo.Employee where LoginName = 'cfloyd' and 1.0 / (Salary - 90731.00) = 0;
+revert;
