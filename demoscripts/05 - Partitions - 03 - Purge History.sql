69d5a122e1f81362b73c2ed1ea766aa2caa7f05e
Allison Benneth <allison@sqltran.org>
2017-04-25 20:28:18 -0500

    Update partitioning demo.

--- demoscripts/05 - Partitions - 03 - Purge History.sql
+++ demoscripts/05 - Partitions - 03 - Purge History.sql
@@ -1,19 +1,63 @@
-use Sales;
-go
-declare @ProductID int;
-declare @ChangePercent float;
+select 'Product' TableName, count(*) NbrRows from Product
+union all
+select 'ProductHistory' TableName, count(*) NbrRows from ProductHistory
+union all
+select 'ProductArchive' TableName, count(*) NbrRows from ProductArchive;
 
-while 0 = 0
-begin
-	select top 1 @ProductID = ProductID
-	from Product
-	order by newid();
+-- Now we'll move one partition at a time into the ProductArchive table.
 
-	select @ChangePercent = 0.5 - (binary_checksum(newid()) + 2147483648.) / 4294967296.;
+begin transaction;
 
-	update Product
-	set CurrentPrice += CurrentPrice * @ChangePercent
-	where ProductID = @ProductID;
+alter table dbo.ProductHistory
+switch partition 1 to dbo.ProductArchive partition 1;
 
-	waitfor delay '0:00:01';
-end
+alter partition scheme schemeProductHistoryPartitionByEndTime
+next used [primary];
+
+-- Add a partition to the ProductHistory table for later inserts.
+alter partition function fnProductHistoryPartitionByEndTime()
+split range ('2017-06-01');
+
+alter partition scheme schemeProductArchivePartitionByEndTime
+next used [primary];
+
+-- Add a partition to the ProductArchive table for data to be loaded in later.
+alter partition function fnProductArchivePartitionByEndTime()
+split range ('2017-04-01');
+
+commit transaction;
+
+select 'Product' TableName, count(*) NbrRows from Product
+union all
+select 'ProductHistory' TableName, count(*) NbrRows from ProductHistory
+union all
+select 'ProductArchive' TableName, count(*) NbrRows from ProductArchive;
+
+----------------------------------------------------------------------------------------------------
+-- One month later, repeat the process
+----------------------------------------------------------------------------------------------------
+
+begin transaction;
+
+alter table dbo.ProductHistory
+switch partition 2 to dbo.ProductArchive partition 2;
+
+alter partition scheme schemeProductHistoryPartitionByEndTime
+next used [primary];
+
+alter partition function fnProductHistoryPartitionByEndTime()
+split range ('2017-07-01');
+
+alter partition scheme schemeProductArchivePartitionByEndTime
+next used [primary];
+
+alter partition function fnProductArchivePartitionByEndTime()
+split range ('2017-05-01');
+
+commit transaction;
+
+select 'Product' TableName, count(*) NbrRows from Product
+union all
+select 'ProductHistory' TableName, count(*) NbrRows from ProductHistory
+union all
+select 'ProductArchive' TableName, count(*) NbrRows from ProductArchive;
