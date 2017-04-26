01fef46d247c26c4785616ab82c896f664d30e65
Allison Benneth <allison@sqltran.org>
2017-04-26 16:08:24 -0500

    Add columnstore scripts.

---
+++ DemoScripts/04 - Columnstore - 01 - Create Database.sql
@@ -0,0 +1,71 @@
+drop database if exists PersonDW;
+go
+create database PersonDW;
+go
+use PersonDW;
+go
+
+drop table if exists PersonDim;
+go
+create table PersonDim
+(
+	PersonID int not null,
+	FirstName nvarchar(30) not null,
+	LastName nvarchar(30) not null,
+	AddressLine1 nvarchar(75) not null,
+	City nvarchar(40) not null,
+	State nvarchar(5) not null,
+	constraint pk_PersonDim primary key clustered (PersonId)
+);
+
+with FirstNames as
+(
+	select FirstName
+	from (values ('Mary'), ('Jeremiah'), ('Lorena'), ('Robert'), ('Carrie'), 
+		('Michael'), ('Wanda'), ('Antoinette'), ('Jarod'), ('Dana')) fn (FirstName)
+), LastNames as
+(
+	select LastName
+	from (values ('Schinke'), ('Weber'), ('Barber'), ('Simmons'), ('Sage'), 
+		('Cureton'), ('Wardlaw'), ('Hermansen'), ('Novick'), ('Carden')) ln (LastName)
+), Streets as
+(
+	select StreetName
+	from (values ('Carew Terr'), ('Morandi St'), ('Huntley Meadows St'), ('Limmings Rd'), ('Sandbed Terr'), 
+		('Cerra Vista Ln'), ('Lenwood Ln'), ('Timbercrest Ln'), ('Holme House Rd'), ('Del Camino Rd')) st (StreetName)
+), StreetNumberPlaceholders as
+(
+	select Placeholder
+	from (values (1), (1)) snp (Placeholder)
+), Cities as
+(
+	select CityName
+	from (values ('Lake Park'), ('Little Valley'), ('Speers'), ('Sykesville'), ('Grand Meadow'), 
+		('Energy'), ('Royalton'), ('Pandora'), ('Pine Bluffs'), ('Princeton')) cn (CityName)
+), States as
+(
+	select State
+	from (values ('AK'), ('AL'), ('AR'), ('AZ'), ('CA'), ('CO'), ('CT'), ('DE'), ('FL'), ('GA'), 
+		('HI'), ('IA'), ('ID'), ('IL'), ('IN'), ('KS'), ('KY'), ('LA'), ('MA'), ('MD'), 
+		('ME'), ('MI'), ('MN'), ('MO'), ('MS'), ('MT'), ('NC'), ('ND'), ('NE'), ('NH'), 
+		('NJ'), ('NM'), ('NV'), ('NY'), ('OH'), ('OK'), ('OR'), ('PA'), ('RI'), ('SC'), 
+		('SD'), ('TN'), ('TX'), ('UT'), ('VA'), ('VT'), ('WA'), ('WI'), ('WV'), ('WY')) st (State)
+), Persons as
+(
+	select
+		newid() PersonGuid,
+		fn.FirstName,
+		ln.LastName,
+		cast(cast(100. + (binary_checksum(newid()) + 2147483648.) / 4294967296. * 900. as int) as nvarchar(10)) + ' ' + st.StreetName AddressLine1,
+		ci.CityName,
+		sta.State
+	from FirstNames fn
+	cross apply LastNames ln
+	cross apply Streets st
+	cross apply StreetNumberPlaceholders snp
+	cross apply Cities ci
+	cross apply States sta
+)
+insert PersonDim (PersonID, FirstName, LastName, AddressLine1, City, State)
+select row_number() over (order by PersonGuid) PersonID, FirstName, LastName, AddressLine1, CityName, State
+from Persons;
---
+++ DemoScripts/04 - Columnstore - 02 - Querying Columnstore.sql
@@ -0,0 +1,51 @@
+use PersonDW;
+go
+set statistics io on;
+set statistics time on;
+
+-- How much space is the current row-based clustered index using?
+exec sp_spaceused 'PersonDim';
+-- Data size = 104832 KB
+
+select *
+from PersonDim pd
+where pd.State = 'TN';
+-- Check output on messages tab.  Requires 13154 logical reads to satisfy the query.
+
+-- Does adding a non-clustered row index help that?
+create index ix1_PersonDim__State on PersonDim (State);
+
+select *
+from PersonDim pd
+where pd.State = 'TN';
+-- No, still requires 13154 logical reads.  The selectivity of the requested data is sufficiently large that the nonclustered index doesn't help amd isn't used.
+
+drop index PersonDim.ix1_PersonDim__State;
+
+-- Convert to columnstore index.
+
+alter table PersonDim drop constraint pk_PersonDim;
+create clustered columnstore index cci_PersonDim on PersonDim;
+
+exec sp_spaceused 'PersonDim';
+-- Data size = 5696 KB  ==> 18x compression!
+
+select *
+from PersonDim pd
+where pd.State = 'TN';
+-- Now requires only 1475 logical reads.
+
+create nonclustered columnstore index nci_PersonDim__Stage on PersonDim (State);
+-- Error, can only create one columnstore index per table
+-- Msg 35339, Level 16, State 1, Line 38
+-- Multiple columnstore indexes are not supported.
+
+alter index cci_PersonDim on PersonDim rebuild with (data_compression = columnstore_archive);
+
+exec sp_spaceused 'PersonDim';
+-- Data size = 5400 KB  -- got a bit better compression
+
+select *
+from PersonDim pd
+where pd.State = 'TN';
+-- Now requires 1354 logical reads.
